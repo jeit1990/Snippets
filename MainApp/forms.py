a57a0cdf0b23c22acdfd4096714179ed64f4d14d
user_ubuntu <phedotovalubov@gmail.com>
2024-09-26 15:53:41 +0300

    1task

---
+++ MainApp/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm, TextInput, Textarea
+from MainApp.models import Snippet
+
+
+class SnippetForm(ModelForm):
+   class Meta:
+       model = Snippet
+       # Описываем поля, которые будем заполнять в форме
+       fields = ['name', 'lang', 'code']
+       labels = {'name': '', 'lang': '', 'code': ''}
+       widgets = {
+           'name': TextInput(attrs={"class":"form-control form-control-lg",
+                                    'style': "max-width: 350px",
+                                    'placeholder': 'Название сниппета'}),
+           'code': Textarea(attrs={"class":"form-control form-control-lg",
+                                    'style': "max-width: 350px",
+                                    'placeholder': 'Код сниппета'})
+       }
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
+
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -8,8 +10,17 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {'pagename': 'Добавление нового сниппета',
+                   'form': form }
+        return render(request, 'pages/add_snippet.html', context)
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets_list")
+        return render(request,'pages/add_snippet.html',{'form': form})
 
 
 def snippets_page(request):
@@ -27,3 +38,42 @@ def get_snippet(request, snippet_id):
     else:
         context = context | {'snippet': snippet}
         return render(request, 'pages/snippet.html', context)
+
+
+
+
+
+def snippet_delete(request, snippet_id):
+    context = {'pagename': 'Удаление сниппета'}
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+        context = context | {'snippet': snippet}
+    except:
+        return render(request, 'pages/error.html', context | {'error': f'not found {snippet_id}'},)
+
+    if request.method == "POST":
+
+        snippet = Snippet.objects.get(id=snippet_id)
+
+        snippet.delete()
+        return redirect("snippets_list")
+    return render(request, 'pages/del_snippet.html', context)
+
+
+def snippet_edit(request, snippet_id):
+    context = {'pagename': 'Редактирование сниппета'}
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+        context = context | {'snippet': snippet}
+    except:
+        return render(request, 'pages/error.html', context | {'error': f'not found {snippet_id}'},)
+    if request.method == "GET":
+        form = SnippetForm(instance=snippet)
+        context = context = context | {'form': form }
+        return render(request, 'pages/edit_snippet.html', context)
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets_list")
+        return render(request,'pages/edit_snippet.html',{'form': form})
