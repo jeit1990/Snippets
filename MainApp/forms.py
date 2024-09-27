cbbd14db2773215631bdb98b1e80dd7463dafb29
user_ubuntu <phedotovalubov@gmail.com>
2024-09-27 12:52:42 +0300

    auth

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -2,11 +2,12 @@ from django.forms import ModelForm, TextInput, Textarea
 from MainApp.models import Snippet
 
 
+
 class SnippetForm(ModelForm):
    class Meta:
        model = Snippet
        # Описываем поля, которые будем заполнять в форме
-       fields = ['name', 'lang', 'code']
+       fields = ['name', 'lang', 'code','public_snippet']
        labels = {'name': '', 'lang': '', 'code': ''}
        widgets = {
            'name': TextInput(attrs={"class":"form-control form-control-lg",
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,30 +1,43 @@
 from django.http import Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
-
+from django.contrib import auth
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
-
+@login_required
 def add_snippet_page(request):
     if request.method == "GET":
         form = SnippetForm()
+
         context = {'pagename': 'Добавление нового сниппета',
                    'form': form }
         return render(request, 'pages/add_snippet.html', context)
     if request.method == "POST":
         form = SnippetForm(request.POST)
         if form.is_valid():
-            form.save()
+            n_snip = form.save(commit=False)
+            if request.user.is_authenticated:
+                n_snip.user = request.user
+                n_snip.save()
             return redirect("snippets_list")
         return render(request,'pages/add_snippet.html',{'form': form})
 
 
+@login_required
+def snippets_page_user(request, user_name):
+    snippets = Snippet.objects.filter(user__username=user_name)
+    context = {'pagename': 'Просмотр сниппетов', 'snippets': snippets}
+    return render(request, 'pages/view_snippets.html', context)
+
+
 def snippets_page(request):
-    snippets = Snippet.objects.all()
+    snippets = Snippet.objects.filter(public_snippet=True)
     context = {'pagename': 'Просмотр сниппетов', 'snippets': snippets}
     return render(request, 'pages/view_snippets.html', context)
 
@@ -40,10 +53,7 @@ def get_snippet(request, snippet_id):
         return render(request, 'pages/snippet.html', context)
 
 
-
-
-
-def snippet_delete(request, snippet_id):
+def snippet_delete_my(request, snippet_id):
     context = {'pagename': 'Удаление сниппета'}
     try:
         snippet = Snippet.objects.get(id=snippet_id)
@@ -57,10 +67,14 @@ def snippet_delete(request, snippet_id):
     return render(request, 'pages/del_snippet.html', context)
 
 
+@login_required
 def snippet_edit(request, snippet_id):
+
     context = {'pagename': 'Редактирование сниппета'}
     try:
         snippet = Snippet.objects.get(id=snippet_id)
+        if request.user != snippet.user:
+            return render(request, 'pages/error.html', context | {'error': f'Нельзя редактировать чужие сниппеты'},)
         context = context | {'snippet': snippet}
     except:
         return render(request, 'pages/error.html', context | {'error': f'not found {snippet_id}'},)
@@ -69,9 +83,51 @@ def snippet_edit(request, snippet_id):
         context = context | {'form': form }
         return render(request, 'pages/edit_snippet.html', context)
     if request.method == "POST":
-        snippet.name = request.POST['name']
+        """snippet.name = request.POST['name']
         snippet.code = request.POST['code']
-        snippet.lang = request.POST['lang']
+        snippet.lang = request.POST['lang']"""
+        data = request.POST
+        for key, value in data.items():
+            setattr(snippet, key, value)
+        """print(request.POST)
+        print(hasattr(request.POST, "public_snippet"))"""
+        try:
+            data["public_snippet"]
+            snippet.public_snippet = True
+        except:
+            snippet.public_snippet = False
         snippet.save()
         return redirect("snippets_list")
         #return render(request,'pages/edit_snippet.html',{'form': form})
+
+
+def snippet_delete(request, snippet_id):
+    if request.method == "GET" or request.method == "POST":
+        snippet = get_object_or_404(Snippet, id=snippet_id)
+        if request.user != snippet.user:
+            return render(request, 'pages/error.html', context | {'error': f'Нельзя редактировать чужие сниппеты'},)
+        snippet.delete()
+        return redirect("snippets_list")
+    #return render(request, 'pages/del_snippet.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+        else:
+            # Return error message
+            context = {
+                'pagename': 'PythonBin',
+                "errors": "wrong username or password"
+            }
+            return render(request, "pages/index.html", context)
+    return redirect('home')
+
+
+def logout(request):
+    auth.logout(request)
+    return  redirect('home')
